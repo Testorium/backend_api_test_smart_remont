b6288b1bac3a8287dcfa5442e33e616a398b8c39
AdiletAY <126655092+AdiletAY@users.noreply.github.com>
2026-01-19 11:02:41 +0500

    feat: add cors

--- app/src/main.py
+++ app/src/main.py
@@ -2,8 +2,10 @@ from contextlib import asynccontextmanager
 from typing import AsyncGenerator
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.routers import main_router
 
+from .config import cors_config
 from .database import db_manager
 from .exception_handlers import register_exception_handlers
 
@@ -15,6 +17,14 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
     await db_manager.dispose()
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(lifespan=lifespan, redirect_slashes=False)
 app.include_router(main_router)
 register_exception_handlers(app)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=cors_config.allow_origins,
+    allow_credentials=cors_config.allow_credentials,
+    allow_methods=cors_config.allow_methods,
+    allow_headers=cors_config.allow_headers,
+)
